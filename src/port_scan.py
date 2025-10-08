b0b51da2425f34a4cd1686cf86e80f9c6fb96c8e
hexadecalice <alicemirga95@gmail.com>
2025-10-08 14:55:41 -0700

    Moved the argparsing stuff outside of the async main function. Now all argument stuff is handled before the event loop begins.

--- src/port_scan.py
+++ src/port_scan.py
@@ -5,6 +5,7 @@ import asyncio
 import time
 import concurrent.futures
 import argparse
+from host_gather import device_scan
 
 #Change testip to the ip you want to scan the ports of
 #Pro-tip: You can use host_gather.py to find IP's on your network to run this tool on
@@ -66,7 +67,9 @@ def scanPort(ip, port):
 
 
 
-
+#This is a wrapper function, that takes our blocking scapy function
+#And makes it asynchronous, using run_in_executor to tell asyncio that the entire function
+#Is in and of itself, something thats blocking
 async def async_scan_wrapper(exec,host,port):
     event_loop = asyncio.get_running_loop()
     port_result = await event_loop.run_in_executor(exec, scanPort, host, port)
@@ -74,29 +77,13 @@ async def async_scan_wrapper(exec,host,port):
 
 
 
-async def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-ip",type=str, help="Specifies the host for target. Either a website or IP address.")
-    parser.add_argument("-p", type=str, help="Specifies the port range, written start,end")
-    parser.add_argument("-t",type=int, help="Sets the maximum number of threads. Default is 50.")
-    args = parser.parse_args()
-
-    #Set arguments to variables to be used by the scanner
-    if args.ip == None:
-        parser.print_help()
-        exit(1)
-    else:
-        target_host = args.ip
-
-    if args.t and args.t != 50:
-        max_threads = args.t
-    else:
-        max_threads = 50
+async def main(host,ports,max_threads,):
     thread_executor = concurrent.futures.ThreadPoolExecutor(max_workers=max_threads)
+
     if args.p:
         #Regex to pull out the numbers from the flags input
         try:
-            formatted_port = re.search(r"(\d+),(\d+)", args.p)
+            formatted_port = re.search(r"(\d+),(\d+)", ports)
             start_port = int(formatted_port.group(1))
             end_port = int(formatted_port.group(2))
             port_list = range(start_port, end_port+1)
@@ -104,10 +91,9 @@ async def main():
             async_tasks = [async_scan_wrapper(thread_executor,target_host,port) for port in port_list]
             #Execute them 'concurrently'
             port_results = await asyncio.gather(*async_tasks)
-
-
         except:
             print(error_message)
+            exit(1)
 
     #Scan common ports if no ports are provided
     else:
@@ -117,4 +103,36 @@ async def main():
     for port, result in port_results:
         printResults(port, result)
 
-asyncio.run(main())
+
+
+
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-ip",type=str, help="Specifies the host for target. Either a website or IP address.")
+parser.add_argument("-p", type=str, help="Specifies the port range, written start,end")
+parser.add_argument("-t",type=int, help="Sets the maximum number of threads. Default is 50.")
+parser.add_argument("-lh", "--local_hosts", help="Prints the IP addresses/Mac addresses of local devices", action="store_true")
+
+args = parser.parse_args()
+#If local host flag is set, scan for local devices then exit the program
+if args.local_hosts:
+    local_host = device_scan(verbose=False)
+    for host in local_host:
+        print("IP Address: %s" % host.get("ip"))
+        print("Mac Address: %s" % host.get("mac"))
+        print("Manufacturer: %s" % host.get("manu"))
+        print("Host Name (Usually undetermined): %s\n" % host.get("host name"))
+    exit(1)
+#Set arguments to variables to be used by the scanner
+if args.ip == None:
+    parser.print_help()
+    exit(1)
+else:
+    target_host = args.ip
+
+if args.t and args.t != 50:
+    max_threads = args.t
+else:
+    max_threads = 50
+asyncio.run(main(target_host,args.p,max_threads))
