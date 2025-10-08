2beeced1a811fc8fb0f01bf01f30056bf7555e18
hexadecalice <alicemirga95@gmail.com>
2025-10-07 17:12:49 -0700

    Revamped the port scanner using asyncio to massively improve performance. Updated the requirements.txt and will be rewriting the host_gather in a similar fashion soon.

--- src/port_scan.py
+++ src/port_scan.py
@@ -1,68 +1,101 @@
 from scapy.all import IP, TCP
 import scapy.all as scapy
 import re
+import asyncio
 #Change testip to the ip you want to scan the ports of
 #Pro-tip: You can use host_gather.py to find IP's on your network to run this tool on
 testip = "your.ip.goes.here"
 
 common_ports = {
-    20: 'FTP/Data',
-    21: 'FTP/Control',
-    22: 'SSH',
-    23: 'Telnet',
-    25: 'SMTP',
-    53: 'DNS',
-    67: 'DHCP/Server',
-    68: 'DHCP/Client',
-    80: 'HTTP',
-    110: 'POP3',
-    123: 'NTP',
-    143: 'IMAP',
-    443: 'HTTPS',
-    445: 'SMB',
-    3306: 'MySQL',
-    3389: 'RDP'
-}
+     20: 'FTP/Data',
+     21: 'FTP/Control',
+     22: 'SSH',
+     23: 'Telnet',
+     25: 'SMTP',
+     53: 'DNS',
+     67: 'DHCP/Server',
+     68: 'DHCP/Client',
+     80: 'HTTP',
+     110: 'POP3',
+     123: 'NTP',
+     143: 'IMAP',
+     443: 'HTTPS',
+     445: 'SMB',
+     3306: 'MySQL',
+     3389: 'RDP'
+ }
 
-def scanPort(ip, portlist=common_ports):
-    for port in portlist:
-        #Craft initial SYN request packet, then send it and listen for one packet
-        synReq = IP(dst=ip)/TCP(dport=port, flags="S")
-        sendSyn = scapy.sr1(synReq, verbose=0, timeout=2)
+def scanPort(ip, port):
+    synReq = IP(dst=ip)/TCP(dport=port, flags="S")
+    sendSyn = scapy.sr1(synReq, verbose=0, timeout=2)
+    #Set conditionals for checking the response packet
+    hasSynAck = sendSyn and sendSyn.haslayer('TCP') and sendSyn['TCP'].flags == 'SA'
+    hasRst = sendSyn and sendSyn.haslayer('TCP') and (sendSyn['TCP'].flags == 'RA')
+    hasAck = sendSyn and sendSyn.haslayer('TCP') and sendSyn['TCP'].flags == 'A'
 
-        #Set conditionals for checking the response packet
-        hasSynAck = sendSyn and sendSyn.haslayer('TCP') and sendSyn['TCP'].flags == 'SA'
-        hasRst = sendSyn and sendSyn.haslayer('TCP') and ("R" in sendSyn['TCP'].flags or sendSyn['TCP'].flags == 'A')
+    if hasSynAck:
+         #Send RST tcp response with correct sequence and acknowledgement numbers to close the connection
+         #We do this to end the connection while the TCP handshake is only half open, this makes it ~stealthier~
+        rstPak = IP(dst=ip)/TCP(sport=synReq['TCP'].sport, dport=port, seq=sendSyn['TCP'].ack, ack=sendSyn['TCP'].seq+1, flags="R")
+        scapy.send(rstPak, verbose=0)
+        return (port, "Open")
+    elif hasRst:
+             return (port, "Closed")
+    elif not sendSyn or hasAck:
+             return (port, "Filtered")
+    else:
+        sendSyn.show()
+        return (port, "Status Unknown")
 
-        if hasSynAck:
-            #Send RST tcp response with correct sequence and acknowledgement numbers to close the connection
-            #We do this to end the connection while the TCP handshake is only half open, this makes it ~stealthier~
-            rstPak = IP(dst=ip)/TCP(sport=synReq['TCP'].sport, dport=port, seq=sendSyn['TCP'].ack, ack=sendSyn['TCP'].seq+1, flags="R")
-            scapy.send(rstPak, verbose=0)
-            print("Port open on port %s (%s)" % (port, portlist[port]))
-        elif hasRst:
-            print("Port closed on port %s " % synReq['TCP'].dport)
-        elif not sendSyn:
-            print("Port %s likely filtered by firewall" % synReq['TCP'].dport)
-        else:
-            print("Cry, I guess.")
-            sendSyn.show()
 
-userPort = input("Enter port range formatted startport,endport\nAlternatively, leave this blank to scan common ports.\n> ")
 
-if userPort:
-    #Regex to pull out the numbers from the userInput
-    #This will still definitely break if you enter wonky input
-    formattedPort = re.search(r"(\d+),(\d+)", userPort)
-    startport = int(formattedPort.group(1))
-    endport = int(formattedPort.group(2))
-    userPortList = {}
-    #Populate a dictionary with the users ports, make sure the common usage ones are still labelled as such
-    for x in range(startport,endport+1):
-        if x in common_ports:
-            userPortList[x] = common_ports[x]d
-        else:
-            userPortList[x] = "No Common Usage"
-    scanPort(testip, userPortList)
-else:
-    scanPort(testip)
+
+async def async_scan_wrapper(host,port):
+    event_loop = asyncio.get_running_loop()
+    port_result = await event_loop.run_in_executor(None, scanPort, host, port)
+    return port_result
+
+
+
+async def main():
+    userPort = input("Enter port range formatted startport,endport\nAlternatively, just press enter to scan common ports.\n> ")
+    if userPort:
+        #Regex to pull out the numbers from the userInput
+        #This will still definitely break if you enter wonky input
+        try:
+            formattedPort = re.search(r"(\d+),(\d+)", userPort)
+            startport = int(formattedPort.group(1))
+            endport = int(formattedPort.group(2))
+
+            #Assign ports to asynchronous function and run them "concurrently"
+            port_list = range(startport, endport+1)
+            async_tasks = [async_scan_wrapper(testip,port) for port in port_list]
+            port_results = await asyncio.gather(*async_tasks)
+
+            for port, result in port_results:
+                if result == "Open":
+                    print("Port %s is open. (Common Usage: %s)" % (port, common_ports.get(port, "Unknown")))
+                elif result == "Closed":
+                    print("Port %s is closed." % port)
+                elif result == "Filtered":
+                    print("Port %s is filtered." % port)
+                elif result == "Status Unknown":
+                    print("Port %s's status is unknown. Please scroll up to find response packet information." % port)
+        except:
+                print("Sorry! I didn't quite catch that, make sure your port is formatted start,end with no spaces!\nPlease restart the application.")
+
+    #Scan common ports if no ports are provided
+    else:
+        async_tasks = [async_scan_wrapper(testip,port) for port in common_ports]
+        port_results = await asyncio.gather(*async_tasks)
+        for port, result in port_results:
+            if result == "Open":
+                print("Port %s is open. (Common Usage: %s)" % (port, common_ports.get(port, "Unknown")))
+            elif result == "Closed":
+                print("Port %s is closed." % port)
+            elif result == "Filtered":
+                print("Port %s is filtered." % port)
+            elif result == "Status Unknown":
+                print("Port %s's status is unknown. Please scroll up to find response packet information." % port)
+
+asyncio.run(main())
