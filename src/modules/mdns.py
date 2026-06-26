d82104543e914bed475344f8405e5a347ea64d5f
hexadecalice <hexadecalice@wonderland.attlocal.net>
2026-06-25 19:33:39 -0700

    Actually uploaded the code so far
    So a bit embarassing, I originally uploaded a truncated version that only had the common services list.
    I don't even know what happened. Went to the file and it was just that, pressed ctrl+z a few times and the code came back.
    Must have accidentally deleted it somehow? Anyways this is the actual code discussed in the initial commit.

--- src/modules/mdns.py
+++ src/modules/mdns.py
@@ -2,6 +2,7 @@ from scapy.all import IP, UDP, DNS, DNSQR, DNSRR, sr1, send
 import socket
 import time
 from utils import utilities
+
 common_services = [
     "_http._tcp.local",     
     "_ssh._tcp.local",      
@@ -13,3 +14,20 @@ common_services = [
     "_telnet._tcp.local",   
     "_ftp._tcp.local"       
 ]
+
+
+mDNS_wrapper = IP(dst="224.0.0.251")/UDP(sport=5353, dport=5353)
+
+easy_way = mDNS_Wrapper/DNS(qr=0, qd=DNSQR(qname="_services._dns-sd._udp.local", qtype))
+
+
+def check_for_response(packet): 
+    if packet.haslayer(DNS) and packet["DNS"].ancount > 0): 
+        dns_answer = packet[DNS].an
+
+
+
+
+
+for service in common_services: 
+    multicast_ping = mDNS_wrapper/DNS(qr=0, qd=DNSQR(qname=service, qtype="PTR"))
