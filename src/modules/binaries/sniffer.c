ea4c4610d8ba5325d20ebeac188702fad04bd317
hexadecalice <hexadecalice@wonderland.attlocal.net>
2026-06-29 20:55:58 -0700

    removed deprecated function from when i was playing around with libpcap

--- src/modules/binaries/sniffer.c
+++ src/modules/binaries/sniffer.c
@@ -15,17 +15,6 @@ void write_packet(u_char *args, struct pcap_pkthdr *packet_header, const u_char 
 
 }
 
-bool check_file(const char *filename) { 
-	FILE *file = fopen(filename, "r"); 
-	if (file != NULL) { 
-		fclose(file);
-		return true; 
-	}
-	else { 
-		return false;
-	}
-
-}
 
 
 int main(void) { 
@@ -35,7 +24,7 @@ int main(void) {
 
     struct pcap_pkthdr hdr; 
     const u_char *packet;
-     char errbuf[PCAP_ERRBUF_SIZE];
+    char errbuf[PCAP_ERRBUF_SIZE];
 
     char* filename = "test.pcap\0";
     pcap_dumper_t *file_handle;
@@ -51,7 +40,7 @@ int main(void) {
 
 
     csession = pcap_open_live(device, BUFSIZ, 1, -1, errbuf);
-    pcap_exists = check_file(filename);
+
 
     file_handle = pcap_dump_open(cession, filename);
   
