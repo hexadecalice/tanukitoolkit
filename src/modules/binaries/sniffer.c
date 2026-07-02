68ebf7a93f6eea567a5510af42d4203be30acfc2
hexadecalice <hexadecalice@wonderland.attlocal.net>
2026-07-01 22:15:55 -0700

    added some comments bc i was reading code that had more comments than mine :(

--- src/modules/binaries/sniffer.c
+++ src/modules/binaries/sniffer.c
@@ -31,8 +31,13 @@ typedef struct {
 
 }Configuration;
 
+
 void write_packet(u_char *args, struct pcap_pkthdr *packet_header, const u_char * packet) { 
+
+        //Recast the u_char pointer to a config struct 
         Configuration* config_file = (Configuration *) args; 
+
+        //Write to pcap and increment packet count
         pcap_dump((u_char*)config_file->file_handle, packet_header, packet);
         (*config_file->packet_count)++;
 
@@ -61,22 +66,25 @@ int main(void) {
 
    
 
-    
+    //get a list of interfaces
     pcap_findalldevs(&network_devices, errbuff);
     if(network_devices == NULL) { 
         printf("Device lookup failed with error: %s", errbuff);
         exit(1);
 
     }
+
     //copy our device name from the linked list 
     device = strdup(network_devices->name);
 
     pcap_freealldevs(network_devices);
 
     
-
-
     csession = pcap_open_live(device, BUFSIZ, 1, -1, errbuff);
+
+    if(csession == NULL) { 
+        printf("Session creation failed with error %s", errbuff);
+    }  
 
 
     file_handle = pcap_dump_open(csession, filename);
@@ -88,11 +96,13 @@ int main(void) {
     };  
   
 
+    //this is annoying but it helps me remember
+    //loop arguments are session handle, time to loop, loopback function, and user variables
     pcap_loop(csession, -1, (pcap_handler) write_packet, (u_char*) &config);
 
     free(device);
 
-    printf("Packets Captured: %i\n", packet_count);
+    printf("\nPackets Captured: %i\n", packet_count);
 
     return 0;   
 }
